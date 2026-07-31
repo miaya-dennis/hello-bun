9080356f06c89e3bed6eea121046a314192699ac
Miaya <myfirefly22@gmail.com>
2026-07-30 22:25:52 -0400

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,5 @@
+let course = "Level 2";
+let message = "Hello from " + course + "!";
+console.log(message);
+console.log("This JavaScript runs on MY machine.");
+console.log("Miaya");
---
+++ notes.md
@@ -0,0 +1,4 @@
+# My Setup
+- Shell: (Git Bash / zsh with oh-my-zsh)
+- Bun version: (from bun --version)
+- Editor: (Notepad++ / BBEdit)
