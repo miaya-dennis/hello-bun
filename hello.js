46f31fe2f79709e6d9ac0b95e17d8d7152dfe7e4
Miaya <myfirefly22@gmail.com>
2026-08-02 19:46:00 -0400

    I changed the original sentence in hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 let course = "Level 2";
 let message = "Hello from" + course + "!";
 console.log(message);
-console.log("This JavaScript run on MY machine.");
+console.log("This is a change I'm making to the document");
 console.log("Miaya");
 console.log("7.31.2026");
